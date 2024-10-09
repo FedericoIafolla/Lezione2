8bc62d033215c58ac71bbe72d346137d3b14366a
FedericoIafolla <iafollafederico@gmail.com>
2024-10-09 16:15:26 +0200

    09/10

--- GestioneEventi/src/main/java/com/evento/TestEvento.java
+++ GestioneEventi/src/main/java/com/evento/TestEvento.java
@@ -6,16 +6,16 @@ public class TestEvento {
     public static void main(String[] args) {
         EventoDAO eventoDAO = new EventoDAO();
 
-        // Creazione di un nuovo evento
-        Evento evento = new Evento("Concerto", new Date(), "Concerto di musica dal vivo", TipoEvento.PUBBLICO, 100);
-        eventoDAO.save(evento);
+        Evento evento1 = new Evento("Concerto", new Date(), "Concerto di musica dal vivo", TipoEvento.PUBBLICO, 100);
+        eventoDAO.save(evento1);
 
-        // Recupero dell'evento
-        Evento recuperato = eventoDAO.getById(evento.getId());
+        Evento evento2 = new Evento("Festa di Compleanno", new Date(), "Festa per il compleanno di Marco", TipoEvento.PRIVATO, 50);
+        eventoDAO.save(evento2);
+
+        Evento recuperato = eventoDAO.getById(evento1.getId());
         System.out.println("Evento recuperato: " + recuperato.getTitolo());
 
-        // Cancellazione dell'evento
-        eventoDAO.delete(evento.getId());
+        eventoDAO.delete(evento1.getId());
         System.out.println("Evento cancellato.");
     }
 }
